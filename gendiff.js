4d739c734fcbf37f274810a029ecb239a0977b29
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-05-06 12:42:15 +0300

    commander -format

--- gendiff.js
+++ gendiff.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
+
 const { program } = require('commander');
 
 program
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .action((filepath1, filepath2, options) => {
+    // функция для сравнения файлов
+  })
+  .on('--help', () => {
+    console.log('');
+    console.log('Examples:');
+    console.log('  $ gendiff --help');
+    console.log('  $ gendiff -h');
+  });
 
-program.parse();
+program.parse(process.argv);
