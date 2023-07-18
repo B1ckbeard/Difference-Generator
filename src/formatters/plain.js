efc313b0aad558ddc74d3aa69f73dd70ca244a9e
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-07-18 17:10:36 +0300

    ref plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,7 +14,9 @@ const getPath = (path, key) => [...path, key];
 
 const plainDiff = (diff, path = []) => {
   const diffLines = diff.map((
-    { type, key, value, children, oldValue, newValue }
+    {
+      type, key, value, children, oldValue, newValue
+    }
   ) => {
     switch (type) {
       case 'added':
