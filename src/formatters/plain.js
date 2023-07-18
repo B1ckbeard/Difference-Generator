3bc13cb4b0d96d3b72a79dda1363fd7304e2285f
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-07-18 16:36:59 +0300

    ref

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,7 +14,10 @@ const getPath = (path, key) => [...path, key];
 
 const plainDiff = (diff, path = []) => {
   const diffLines = diff.map((
-    { type, key, value, children, oldValue, newValue }) => {
+    {
+       type, key, value, children, oldValue, newValue 
+      }
+    ) => {
     switch (type) {
       case 'added':
         return `Property '${getPath(path, key).join('.')}' was added with value: ${stringify(value)}`;
