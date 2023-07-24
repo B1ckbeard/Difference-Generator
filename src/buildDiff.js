71a348e77631c0d51f8b2754128578ec77c878d9
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-07-24 09:34:29 +0300

    ref

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -5,20 +5,21 @@ const buildDiff = (data1, data2) => {
   const sortedKeys = _.sortBy(keys);
 
   return sortedKeys.map((key) => {
-    let result;
     if (!_.has(data1, key)) {
-      result =  { key, value: data2[key], type: 'added' };
+      return { key, value: data2[key], type: 'added' };
     }
     if (!_.has(data2, key)) {
-      result =  { key, value: data1[key], type: 'deleted' };
+      return { key, value: data1[key], type: 'deleted' };
     }
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      result =  { key, children: buildDiff(data1[key], data2[key]), type: 'nested' };
+      return { key, children: buildDiff(data1[key], data2[key]), type: 'nested' };
     }
     if (_.isEqual(data1[key], data2[key])) {
-      result =  { key, value: data1[key], type: 'unchanged' };
+      return { key, value: data1[key], type: 'unchanged' };
     }
-    return result;
+    return {
+      key, oldValue: data1[key], newValue: data2[key], type: 'changed',
+    };
   });
 };
 
